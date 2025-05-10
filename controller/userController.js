e14f114af136ca556d29a69524d8d0f358ddc283
lenin12345-dev <leninmohapatra@gmail.com>
2025-05-10 18:41:46 -0400

    refresh handler add

--- controller/userController.js
+++ controller/userController.js
@@ -120,6 +120,34 @@ exports.userSignUp = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+exports.handleRefreshToken = async (req, res) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(401); // Unauthorized
+
+  const refreshToken = cookies.jwt;
+
+  const foundUser = await User.findOne({ refreshToken }).exec();
+  if (!foundUser) return res.sendStatus(403); // Forbidden
+
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || foundUser.username !== decoded.username) return res.sendStatus(403);
+
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          id: foundUser._id,
+          username: foundUser.username,
+          role: foundUser.role,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" } // shorter access token
+    );
+
+    res.json({ accessToken });
+  });
+};
+
 
 exports.getUserProfile = async (req, res) => {
   try {
